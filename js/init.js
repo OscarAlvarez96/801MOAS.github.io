f50e48d01e22db33ccd09ce4912b9790d36a0cc0
OscarAlvarez96 <93893268+OscarAlvarez96@users.noreply.github.com>
2021-11-10 12:36:45 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyBQgq9xlFJv3aIMCgowVhm5R59u_YKd5E8",
-  authDomain: "fbauten801.firebaseapp.com",
-  projectId: "fbauten801",
-  storageBucket: "fbauten801.appspot.com",
-  messagingSenderId: "39321023546",
-  appId: "1:39321023546:web:6ee7230d0b7c6f985fb103",
-  measurementId: "G-QGG61H2FB7"
+ apiKey: "AIzaSyDFhpVpFsz8jniG-Vl8-ZLXYtPMqrDmfVs",
+  authDomain: "moas-4be8a.firebaseapp.com",
+  projectId: "moas-4be8a",
+  storageBucket: "moas-4be8a.appspot.com",
+  messagingSenderId: "160256643574",
+  appId: "1:160256643574:web:d41163a1dcc399f49977d7",
+  measurementId: "G-X43MC7VBJN"
     });
